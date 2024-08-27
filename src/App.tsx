6588a3990b5f1b3ea099fa9211aecb487f1bd6e8
ankitmaurya <ankitmaurya2989@gmail.com>
2024-08-27 21:35:58 +0530

    commit1

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="text-red-700 bg-blue-800">
+      sggngfmnm
+      <p className="text-lg text-red-800">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quo
+        adipisci atque corporis, nam hic consectetur laudantium. Nihil aperiam,
+        magni praesentium culpa illum doloribus adipisci, molestiae saepe
+        suscipit fugiat ipsam accusamus atque blanditiis distinctio.
+      </p>
+      <button className="text-red bg-red-700">count</button>
+      hfdfd
+    </div>
+  );
+}
+
+export default App;
